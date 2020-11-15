c9a74b3dedfa3666e40bd3270d6a0dc8f52f7eb7
besem <besemilie@gmail.com>
2020-11-15 18:19:43 +0100

    Init: beginning of the project

---
+++ api/src/ChocolateBar.ts
@@ -0,0 +1,11 @@
+export interface ChocolateBar{
+    company: string,
+    name: string,
+    date: string,
+    cocoa_percent: number,
+    company_location: string,
+    rating: number,
+    bean_origin: string,
+    bean_typ: string,
+    ref: number,
+}
